b3ce6bfc6b8979ecf57e60e5cd7f1eaf176726e3
Jan Sulejmani <64248502+jansulejmani@users.noreply.github.com>
2023-03-10 14:37:15 -0600

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jan Sulejmani on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
